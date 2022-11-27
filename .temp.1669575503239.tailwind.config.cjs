13596df658f02f5734f9457272da4e7ec24661ab
Rangerdevv <meerabdul2008@gmail.com>
2022-11-27 14:19:28 -0500

    fixed some animations

---
+++ .temp.1669575503239.tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: {
+          files: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue,mdx,md}'],
+          // transform: {
+          //   mdx: (content) => {
+          //     return remark().process(content)
+          //   }}
+      },
+  //content: ["./src/components/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [
+    require("tailwindcss-fluid-type"),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
